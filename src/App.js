911a4568bd9b73d2737bc47c21513380d4a20932
FaizaanFazal <dkfaizaan12@gmail.com>
2023-11-25 15:03:49 +0500

    added editorjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import EditorJS from "@editorjs/editorjs";
 import './App.css';
+import { useEffect, useRef } from "react";
 
 function App() {
+
+  const ejInstance = useRef();
+
+  const initEditor = () => {
+    const editor = new EditorJS({
+      holder: 'editorjs',
+      onReady: () => {
+        ejInstance.current = editor;
+      },
+      autofocus: true,
+      data: DEFAULT_INITIAL_DATA,
+      onChange: async () => {
+        let content = await editor.saver.save();
+        console.log(content);
+      },
+      tools: {
+        header: Header,
+      },
+    });
+  };
+
+
+  useEffect(() => {
+    if (ejInstance.current === null) {
+      initEditor();
+    }
+
+    return () => {
+      ejInstance?.current?.destroy();
+      ejInstance.current = null;
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="editorjs">
+      hello
     </div>
   );
 }
