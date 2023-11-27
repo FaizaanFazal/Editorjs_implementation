0839dbcfbe0b648c7ecb3cd0f934fe458574fbc0
Faizaan <dkfaizaan12@gmail.com>
2023-11-27 19:56:11 +0500

    added tools

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import EditorJS from "@editorjs/editorjs";
 import './App.css';
 import { useEffect, useRef } from "react";
-import Header from '@editorjs/header'; 
+import Header from '@editorjs/header';
+import Quote from '@editorjs/quote';
+import SimpleImage from "@editorjs/simple-image";
+import NestedList from '@editorjs/nested-list';
+import Checklist from '@editorjs/checklist'
+import LinkTool from '@editorjs/link';
+import Embed from '@editorjs/embed';
+
 
 function App() {
- 
-  const DEFAULT_INITIAL_DATA =  {
+
+  const DEFAULT_INITIAL_DATA = {
     "time": new Date().getTime(),
     "blocks": [
       {
@@ -16,7 +23,7 @@ function App() {
         }
       },
     ]
-}
+  }
 
   const ejInstance = useRef();
 
@@ -32,8 +39,30 @@ function App() {
         let content = await editor.saver.save();
         console.log(content);
       },
+
       tools: {
         header: Header,
+        quote: Quote,
+        image: SimpleImage, //configuration not done
+        list: {
+          class: NestedList,
+          inlineToolbar: true,
+          config: {
+            defaultStyle: 'unordered'
+          },
+        },
+        checklist: {
+          class: Checklist,
+          inlineToolbar: true,
+        },
+        linkTool: {
+          class: LinkTool, //to do
+          config: {
+            endpoint: "https://codex.so/public/app/img/meta_img.png", // Your backend endpoint for url data fetching,
+          }
+        },
+        embed: Embed, //todo
+
       },
     });
   };
